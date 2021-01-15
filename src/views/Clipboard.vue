0d02b9666c520830d3df041b4c4226600cbb35e4
Deji <wrecodde@gmail.com>
2021-01-15 22:47:15 +0100

    --amend

---
+++ src/views/Clipboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="container">
+    <h1>On Clipboard</h1>
+    <c-button>
+      <router-link to="/">Back home</router-link>
+    </c-button>
+  </div>
+</template>
+
+<script>
+import {
+  CButton
+}
+from "@chakra-ui/vue"
+
+
+export default {
+  naem: "About",
+  components: {
+    CButton
+  }
+}
+</script>
